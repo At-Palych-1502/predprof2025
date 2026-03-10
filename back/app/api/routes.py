b1c0fad0b95b4e988b551059a2373916c413953d
xchose31 <rte3001@vk.com>
2026-03-10 14:37:34 +0300

    commit

--- back/app/api/routes.py
+++ back/app/api/routes.py
@@ -2,10 +2,15 @@ from flask import jsonify, request, Blueprint
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from app import db
 from app.models import Dot
-from ..utils_api import get_all_titles
+from ..utils_api import get_all_titles, get_data
 
 bp = Blueprint('routes', __name__)
 
 @bp.route('/map')
 def map():
     return jsonify({"message": {"data": get_all_titles('')}})
+
+
+@bp.route('/stations')
+def stations():
+    return jsonify({"message": {"data": [[5, 10, 'high32'], [15, 45, 'high64'], [1, 1, 'station'], [56, 62, 'station']]}})
--- back/run.py
+++ back/run.py
@@ -4,4 +4,4 @@ app = create_app()
 
 if __name__ == '__main__':
     # debug=True включаем только для разработки
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='localhost', port=5000, debug=True)
